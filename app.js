137a73112f576f65ca568f1c712e1066345b9861
SHIH-WEI-LIU <lsw850718@yahoo.com>
2022-07-28 16:34:00 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose') // 載入 mongoose
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const exphbs = require('express-handlebars');
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 // 取得資料庫連線狀態
 const db = mongoose.connection
 // 連線異常
@@ -14,9 +18,10 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-// 設定首頁路由
+// 設定路由
+// Todo 首頁
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 // 設定 port 3000
