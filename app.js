9ba7f405ff5f8df667875f86832d94096a6a1cd2
SHIH-WEI-LIU <lsw850718@yahoo.com>
2022-07-28 21:47:44 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
-// 載入 express 並建構應用程式伺服器
+//串接模組
 const express = require('express')
-const app = express()
-const mongoose = require('mongoose') // 載入 mongoose
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const exphbs = require('express-handlebars');
+const mongoose = require('mongoose')
+const Todo = require('./models/todo')
+const exphbs = require('express-handlebars')
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
-// 取得資料庫連線狀態
+const app = express()
+
+//mongoDB連線設定
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
-// 連線異常
 db.on('error', () => {
   console.log('mongodb error!')
 })
-// 連線成功
 db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-// 設定路由
-// Todo 首頁
+//樣版引擎
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+//撈資料庫資料
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
 })
 
-// 設定 port 3000
+//監聽網頁
 app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+  console.log('App is running on http://localhost:3000.')
 })
