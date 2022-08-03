ddf0410a8f660b6045819e5129eeee62737444db
SHIH-WEI-LIU <lsw850718@yahoo.com>
2022-08-03 23:24:19 +0800

    refactor: modify express routes to separation of concerns

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 //串接模組
 const express = require('express')
 const mongoose = require('mongoose')
-const Todo = require('./models/todo') //todo.js的模組
 const exphbs = require('express-handlebars') //樣版引擎
 const bodyParser = require('body-parser') // 引用 body-parser(用來抓取res.body)
 const methodOverride = require('method-override') // 載入 method-override
+// 引用路由器
+const routes = require('./routes') //引入路由器時，路徑設定為 /routes 就會自動去尋找目錄下叫做 index 的檔案
 
 const app = express()
 
@@ -25,70 +26,8 @@ app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
-
-//路由設定
-//瀏覽所有todo(首頁)
-app.get('/', (req, res) => {
-  Todo.find() // 取出 Todo model 裡的所有資料
-    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .sort({ name: 'asc' }) 
-    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
-    .catch(error => console.error(error)) // 錯誤處理
-})
-
-//設定new.hbs頁面
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
-
-//設定post路由（要記得設定body-parser）(todos 是因為<form action="/todos" method="POST">)
-app.post('/todos', (req, res) => {
-  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
-  return Todo.create({ name })     // 存入資料庫
-    .then(() => res.redirect('/')) // 新增完成後導回首頁
-    .catch(error => console.log(error))
-})
-
-//瀏覽特定 To-do詳細資料的路由
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id) //從資料庫查找特定資料
-    .lean()
-    .then((todo) => res.render('detail', { todo }))
-    .catch(error => console.log(error))
-})
-
-//編輯todo資料頁面的路由
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('edit', { todo }))
-    .catch(error => console.log(error))
-})
-
-//修改特定 todo 資料的路由
-app.put('/todos/:id', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
-
-//刪除todo的路由
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+// 將 request 導入路由器
+app.use(routes) 
 
 //監聽網頁
 app.listen(3000, () => {
