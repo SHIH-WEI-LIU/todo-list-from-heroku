271074a3fb776d4b607d08cff9fa6dbb1a139bdb
SHIH-WEI-LIU <lsw850718@yahoo.com>
2022-08-03 23:45:49 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
 //串接模組
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars') //樣版引擎
 const bodyParser = require('body-parser') // 引用 body-parser(用來抓取res.body)
 const methodOverride = require('method-override') // 載入 method-override
-// 引用路由器
 const routes = require('./routes') //引入路由器時，路徑設定為 /routes 就會自動去尋找目錄下叫做 index 的檔案
+require('./config/mongoose') //載入mongoose
 
 const app = express()
-
-//mongoDB連線設定
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 //樣版引擎
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
